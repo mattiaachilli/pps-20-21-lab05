edb7d0ee0b05459eb620616323f136596db63ea1
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-04-11 14:25:03 +0200

    red-green-refactor performanceVectorVsArrayVsArrayBuffer

--- test/u05lab/CollectionTest.scala
+++ test/u05lab/CollectionTest.scala
@@ -4,7 +4,7 @@ import u05lab.code.PerformanceUtils
 import org.junit.jupiter.api.Assertions.{assertEquals, assertThrows}
 import org.junit.jupiter.api.Test
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 class CollectionTest {
   import PerformanceUtils._
@@ -12,9 +12,21 @@ class CollectionTest {
   @Test def performanceListVsListBuffer(): Unit = {
     val numberList: List[Int] = List(10, 20, 30 ,40)
     val numberListBuffer: ListBuffer[Int] = ListBuffer(10, 20, 30, 40)
-    assert(measure("ListBuffer add")(numberListBuffer.+=(5)) > measure("List add")(numberList.+:(5)))
-    assert(measure("ListBuffer size")(numberListBuffer.size) < measure("List size")(numberList.size))
+    assert(measure("ListBuffer add")(numberListBuffer.+=(50)) > measure("List add")(numberList.+:(50)))
     assert(measure("ListBuffer get")(numberListBuffer(1)) > measure("List get")(numberList(1)))
-    assert(measure("ListBuffer delete")(numberListBuffer.-=(5)) > measure("List delete")(numberList.reverse.drop(1)))
+    assert(measure("ListBuffer delete")(numberListBuffer.-=(50)) > measure("List delete")(numberList.dropRight(1)))
+    assert(measure("ListBuffer size")(numberListBuffer.size) < measure("List size")(numberList.size))
+  }
+
+  @Test def performanceVectorVsArrayVsArrayBuffer(): Unit = {
+    val numberVector: Vector[Int] = Vector(10, 20, 30 ,40)
+    val numberArray: Array[Int] = Array(10, 20, 30, 40)
+    val numberArrayBuffer: ArrayBuffer[Int] = ArrayBuffer(10, 20, 30, 40)
+    assert(measure("Vector add")(numberVector.+:(50)) < measure("ArrayBuffer add")(numberArrayBuffer.+:(50)))
+    assert(measure("Vector get")(numberVector(1)) > measure("Array get")(numberArray(1)))
+    assert(measure("Array get")(numberArray(1)) < measure("ArrayBuffer get")(numberArrayBuffer(1)))
+    assert(measure("ArrayBuffer delete")(numberArrayBuffer.-(numberArrayBuffer.last)) > measure("Vector delete")(numberVector.dropRight(1)))
+    assert(measure("Array size")(numberArray.length) < measure("ArrayBuffer size")(numberArrayBuffer.length))
+    assert(measure("Vector size")(numberVector.size) > measure("Array size")(numberArray.length))
   }
 }

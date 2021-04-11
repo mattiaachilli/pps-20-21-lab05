2888742cd0a1aefd989515c8c2745a05ea63aba4
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-04-11 14:47:09 +0200

    red-green-refactor performanceSet

--- test/u05lab/CollectionTest.scala
+++ test/u05lab/CollectionTest.scala
@@ -1,7 +1,6 @@
 package u05lab
 
 import u05lab.code.PerformanceUtils
-import org.junit.jupiter.api.Assertions.{assertEquals, assertThrows}
 import org.junit.jupiter.api.Test
 
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
@@ -12,7 +11,7 @@ class CollectionTest {
   @Test def performanceListVsListBuffer(): Unit = {
     val numberList: List[Int] = List(10, 20, 30 ,40)
     val numberListBuffer: ListBuffer[Int] = ListBuffer(10, 20, 30, 40)
-    assert(measure("ListBuffer add")(numberListBuffer.+=(50)) > measure("List add")(numberList.+:(50)))
+    assert(measure("ListBuffer add")(numberListBuffer.+=(50)) < measure("List add")(numberList.+:(50)))
     assert(measure("ListBuffer get")(numberListBuffer(1)) > measure("List get")(numberList(1)))
     assert(measure("ListBuffer delete")(numberListBuffer.-=(50)) > measure("List delete")(numberList.dropRight(1)))
     assert(measure("ListBuffer size")(numberListBuffer.size) < measure("List size")(numberList.size))
@@ -28,5 +27,14 @@ class CollectionTest {
     assert(measure("ArrayBuffer delete")(numberArrayBuffer.-(numberArrayBuffer.last)) > measure("Vector delete")(numberVector.dropRight(1)))
     assert(measure("Array size")(numberArray.length) < measure("ArrayBuffer size")(numberArrayBuffer.length))
     assert(measure("Vector size")(numberVector.size) > measure("Array size")(numberArray.length))
+  }
+
+  @Test def performanceSet(): Unit = {
+    val immutableSet: Set[Int] = Set(10 ,20 ,30 ,40 ,50)
+    val mutableSet: collection.mutable.Set[Int] = collection.mutable.Set(10 ,20 ,30 ,40 ,50)
+
+    assert(measure("Immutable set add")(immutableSet.+(60)) > measure("Mutable set add")(mutableSet.+=(60)))
+    assert(measure("Immutable set delete")(immutableSet.-(60)) < measure("Mutable set delete")(mutableSet.-=(60)))
+    assert(measure("Immutable set contains")(immutableSet.contains(60)) < measure("Mutable set contains")(mutableSet.contains(60)))
   }
 }
